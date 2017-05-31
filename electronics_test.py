830c1f4c384d302def8a64ff75306a96d3984386
gabriel <gabrielburnworth@users.noreply.github.com>
2017-05-30 21:25:06 -0700

    v1.1 continued

--- electronics_test.py
+++ electronics_test.py
@@ -54,6 +54,11 @@ class FarmduinoTestSuite(object):
         self.abridged = False
         self.board = None
         self.firmware = None
+        self.verbose = True
+        if self.verbose:
+            self.newline = '\n'
+        else:
+            self.newline = ''
 
     def connect_to_board(self):
         '''Connect to the board.'''
@@ -90,30 +95,32 @@ class FarmduinoTestSuite(object):
         if self.skip('set parameters'.upper()):
             return
         parameters = {
-            '11': 120, '12': 120, '13': 120,  # movement timeout
-            '15': 0, '16': 0, '17': 0,  # keep active
-            '18': 0, '19': 0, '20': 0,  # home at boot
-            '25': 0, '26': 0, '27': 0,  # enable endstops
-            '31': 0, '32': 0, '33': 0,  # invert motor
-            '36': 1, '37': 0,  # second x axis motor
-            '41': 500, '42': 500, '43': 500,  # acceleration
-            '45': 0, '46': 0, '47': 0,  # stop at home
-            '51': 0, '52': 0, '53': 0,  # negatives
-            '61': 50, '62': 50, '63': 50,  # min speed
-            '71': 800, '72': 800, '73': 800,  # max speed
-            '101': 1, '102': 1, '103': 1,  # encoders
-            '105': 0, '106': 0, '107': 0,  # encoder type
-            '111': 10, '112': 10, '113': 10,  # missed steps
-            '115': 100, '116': 100, '117': 100,  # scaling
-            '121': 10, '122': 10, '123': 10,  # decay
-            '125': 0, '126': 0, '127': 0,  # positioning
-            '131': 0, '132': 0, '133': 0,  # invert encoders
-            '141': 0, '142': 0, '143': 0,  # axis length
-            '145': 0, '146': 0, '147': 0  # stop at max
+            11: 120, 12: 120, 13: 120,  # movement timeout
+            15: 0, 16: 0, 17: 0,  # keep active
+            18: 0, 19: 0, 20: 0,  # home at boot
+            25: 0, 26: 0, 27: 0,  # enable endstops
+            31: 0, 32: 0, 33: 0,  # invert motor
+            36: 1, 37: 0,  # second x axis motor
+            41: 500, 42: 500, 43: 500,  # acceleration
+            45: 0, 46: 0, 47: 0,  # stop at home
+            51: 0, 52: 0, 53: 0,  # negatives
+            61: 50, 62: 50, 63: 50,  # min speed
+            71: 800, 72: 800, 73: 800,  # max speed
+            101: 1, 102: 1, 103: 1,  # encoders
+            105: 0, 106: 0, 107: 0,  # encoder type
+            111: 10, 112: 10, 113: 10,  # missed steps
+            115: 100, 116: 100, 117: 100,  # scaling
+            121: 10, 122: 10, 123: 10,  # decay
+            125: 0, 126: 0, 127: 0,  # positioning
+            131: 0, 132: 0, 133: 0,  # invert encoders
+            141: 0, 142: 0, 143: 0,  # axis length
+            145: 0, 146: 0, 147: 0  # stop at max
             }
         if self.abridged:
             parameters = {'101': 1, '102': 1, '103': 1}  # encoders
-        for parameter, value in parameters.items():
+        for parameter, value in sorted(parameters.items()):
+            print('Set parameter {} to {}: '.format(parameter, value),
+                  end=self.newline)
             self.send_command('F22 P{} V{}'.format(parameter, value))
             self.send_command('F21 P{}'.format(parameter),
                               expected='P{} V{}'.format(parameter, value),
@@ -145,59 +152,107 @@ class FarmduinoTestSuite(object):
     def send_command(self, command, expected=None, test_type='misc',
                      delay=0.25):
         '''Send a command and print the output.'''
+        command_io = {'command': command, 'marker': None, 'expected': expected,
+                      'received': None, 'out': '', 'output': None,
+                      'result': 'FAIL'}
         if expected is not None:  # count as a test
             self.update_test_results('count', test_type)
         # Clear input buffer
         self.ser.reset_input_buffer()
         # Send the command
-        print('{:11}{}'.format('SENDING:', command))
+        if self.verbose:
+            print('{:11}{}'.format('SENDING:', command_io['command']))
         self.ser.write(command + '\r\n')
         # prep for receiving output
-        marker = self.get_response_marker(command)
-        out = ''
-        output = None
+        command_io['marker'] = self.get_response_marker(command)
         time.sleep(delay)
         while self.ser.in_waiting > 0:
-            out += self.ser.read()
+            command_io['out'] += self.ser.read()
 
         if test_type == 'movement':
-            markers = [marker, 'R84']
+            markers = [command_io['marker'], 'R84']
             indent = ' ' * 2
         else:
-            markers = [marker]
+            markers = [command_io['marker']]
             indent = ''
         for i, marker in enumerate(markers):
             if test_type == 'movement':
                 if i == 0:
-                    print('Motor:')
+                    print('Motor: ', end=self.newline)
                 else:
-                    print('Encoder:')
+                    print('Encoder: ', end=self.newline)
 
-            if out != '':  # received output
-                print('{}{:11}'.format(indent, 'RECEIVED:'), end='')
-                ret = out.split('\r\n')[::-1]  # sort output (latest first)
-                for line in ret:
-                    if marker in line:  # response to command sent
-                        print(line)
-                        # discard marker and `Q`
-                        output = (' ').join(line.split(' ')[1:-1])
-                        print('{}{:11}'.format(indent, 'VALUE(S):'), end='')
-                        print(output)  # value in response
-                        break
+            if command_io['out'] != '':  # received output
+                command_io = self.reduce_response(command_io, marker)
 
-            if expected is not None:  # record results of test
-                print('{}{:11}'.format(
-                    indent,
-                    '{:11}{}'.format('EXPECTED:', expected)))
-                if expected in output:  # sucess
-                    result = 'PASS'
+            if command_io['expected'] is not None:  # record results of test
+                if self.compare(command_io['output'], command_io['expected']):
+                    command_io['result'] = 'PASS'
                     self.update_test_results('passed', test_type)
                 else:
-                    result = 'FAIL'
-                print('{}{:11}{}'.format(indent, 'RESULT:', result))
+                    command_io['result'] = 'FAIL'
 
-        print()
-        return output
+            self.print_command_io(command_io, indent)
+
+        return command_io['output']
+
+    def print_command_io(self, command_io, indent):
+        '''Print sent/received for command.'''
+        if command_io['result'] == 'FAIL' or self.verbose:
+            if command_io['received'] is not None:
+                print('{}{:11}{}'.format(
+                    indent, 'RECEIVED:', command_io['received']))
+            if command_io['output'] is not None:
+                print('{}{:11}{}'.format(
+                    indent, 'VALUE(S):', command_io['output']))
+            if command_io['expected'] is not None:
+                print('{}{:11}'.format(
+                    indent,
+                    '{:11}{}'.format('EXPECTED:', command_io['expected'])))
+            if (command_io['result'] is not None
+                    and command_io['received'] is not None):
+                print('{}{:11}{}'.format(
+                    indent, 'RESULT:', command_io['result']))
+                print()
+        elif not self.verbose:
+            if (command_io['result'] is not None
+                    and command_io['received'] is not None):
+                print('{}{}'.format(indent, command_io['result']))
+
+    @staticmethod
+    def reduce_response(command_io, marker):
+        '''Get the correct response and return the value.'''
+        ret = command_io['out'].split('\r\n')[::-1]  # sort output (last first)
+        for line in ret:
+            if marker in line:  # response to command sent
+                command_io['received'] = line
+                # discard marker and `Q`
+                command_io['output'] = (' ').join(
+                    command_io['received'].split(' ')[1:-1])
+                break
+        return command_io
+
+    @staticmethod
+    def compare(output, expected):
+        '''Compare output to expected value.'''
+        outcome = False
+        compare_value = expected.split('V')[-1]
+        if any(op in compare_value for op in ['<', '>', '=']):
+            operator = compare_value[0]
+            value = int(compare_value[1:])
+            if '>' in operator:
+                if output > value:
+                    outcome = True
+            elif '<' in operator:
+                if output < value:
+                    outcome = True
+            elif '=' in operator:
+                if output == value:
+                    outcome = True
+        else:
+            if expected in output:
+                outcome = True
+        return outcome
 
     def print_results(self):
         '''Print number of passing tests.'''
@@ -224,12 +279,12 @@ class FarmduinoTestSuite(object):
         '''Misc tests.'''
         if self.skip(title='Preliminary tests:'.upper()):
             return
-        print('Return firmware version:')
+        print('Return firmware version: ', end=self.newline)
         if not self.skip():
             self.firmware = self.send_command(
                 'F83', expected='GENESIS.V.01.13.EXPERIMENTAL')
 
-        print('Return current position:')
+        print('Return current position: ', end=self.newline)
         if not self.skip():
             self.send_command('F82', expected='X0 Y0 Z0')
 
@@ -280,29 +335,24 @@ class FarmduinoTestSuite(object):
                     text_value = 'on'
                 else:
                     text_value = 'off'
-                print('Turn pin {} {}:'.format(pin, text_value))
+                print('Turn pin {} {}: '.format(pin, text_value),
+                      end=self.newline)
                 if self.skip():
                     continue
                 self.send_command('F41 P{} V{} M0'.format(pin, value))
                 read_pin(pin, value, mode)
         # Read-only pins
-        if self.board == 'RAMPS':
-            read_pins = [
-                {'description': 'soil sensor',
-                 'number': 59, 'mode': 1, 'expected_value': 0},
-                {'description': 'tool verification (apply 5V)',
-                 'number': 63, 'mode': 0, 'expected_value': 1}
-                ]
-        else:  # change to Farmduino pin numbers
-            for pin in read_pins:
-                if pin['description'] == 'soil sensor':
-                    pin['number'] = 5
-                if pin['description'] == 'tool verification (apply 5V)':
-                    pin['number'] = 9
+        read_pins = [
+            {'description': 'soil sensor',
+             'number': 59, 'mode': 1, 'expected_value': '>1000'},
+            {'description': 'tool verification (apply 5V)',
+             'number': 63, 'mode': 0, 'expected_value': 1}
+            ]
         for pin in read_pins:
             mode_text = {'0': 'digital', '1': 'analog'}[str(pin['mode'])]
-            print('Read pin {} - {} - {} read mode:'.format(
-                pin['number'], pin['description'], mode_text))
+            print('Read pin {} - {} - {} read mode: '.format(
+                pin['number'], pin['description'], mode_text),
+                  end=self.newline)
             if not self.skip():
                 read_pin(pin['number'], pin['expected_value'], pin['mode'])
 
